1a551077a2b1656db1a5e6be21e355eb29d85c06
odas <zwtao106@gmail.com>
2021-09-20 15:41:43 +1000

    add the whole project

---
+++ src/main/java/com/odas/SpringbootManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.odas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootManagementSystemApplication.class, args);
+    }
+
+}
