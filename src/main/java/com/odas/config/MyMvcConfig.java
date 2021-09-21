6e3aad78cd5da49f0284fae3eaf7743c79e6a892
odas <zwtao106@gmail.com>
2021-09-22 03:14:20 +1000

    i18n

---
+++ src/main/java/com/odas/config/MyMvcConfig.java
@@ -0,0 +1,26 @@
+package com.odas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // 这里自己定义视图解析器，输入/或者/index.html跳转到templates下的login页面
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+
+    // 自定义的国际化组件加在这里哟
+    // 这里我们在configuration自己配置了LocaleResolver所以springboot就会自动去装配他
+    // 进行国际化，源码里已经配置好里所以这个方法必须返回LocalResolver类型才能实现国际化自定义
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
+    }
+}
---
+++ src/main/java/com/odas/controller/IndexController.java
@@ -0,0 +1,9 @@
+package com.odas.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+
+}
