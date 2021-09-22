6ab3729b232e531bbd17715c71f9c3582928fccb
Aiden Tao <zwtao99@outlook.com>
2021-09-23 02:14:05 +1000

    update controller

--- src/main/java/com/odas/controller/LoginController.java
+++ src/main/java/com/odas/controller/LoginController.java
@@ -1,5 +1,27 @@
 package com.odas.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class LoginController {
+
+    @RequestMapping("/user/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Model model) {
+        // 具体的业务
+        if (StringUtils.hasText(username) && "123456".equals(password)) {
+            return "dashboard";
+        } else {
+            // 告诉用户，你登陆失败了
+            model.addAttribute("msg", "用户名或者密码错误!");
+            return "index";
+        }
+    }
+
+
 
 }
